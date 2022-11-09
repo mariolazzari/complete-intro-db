cb69102f2e508ee1a6be663a68a64192052fafb9
Mario Lazzari <mario.lazzari@gmail.com>
2022-11-09 20:17:23 +0100

    Done lesson 3 chapter 2

---
+++ nosql/pets.js
@@ -0,0 +1,29 @@
+db.pets.insertMany(
+  Array.from({ length: 10000 }).map((_, index) => ({
+    name: [
+      "Luna",
+      "Fido",
+      "Fluffy",
+      "Carina",
+      "Spot",
+      "Beethoven",
+      "Baxter",
+      "Dug",
+      "Zero",
+      "Santa's Little Helper",
+      "Snoopy",
+    ][index % 9],
+    type: ["dog", "cat", "bird", "reptile"][index % 4],
+    age: (index % 18) + 1,
+    breed: [
+      "Havanese",
+      "Bichon Frise",
+      "Beagle",
+      "Cockatoo",
+      "African Gray",
+      "Tabby",
+      "Iguana",
+    ][index % 7],
+    index: index,
+  }))
+);
